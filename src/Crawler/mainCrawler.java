b36c334a972c0ce3e3b33658664efd6f3973d1a4
Wo-oJin <dnwls813@ajou.ac.kr>
2021-09-05 21:12:51 +0900

    Add Ajou University Portal Autologin Function

--- src/Crawler/mainCrawler.java
+++ src/Crawler/mainCrawler.java
@@ -15,28 +15,36 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+ 
 public class mainCrawler {
 
+
+    //WebDriver
     private static WebDriver driver;
     private static ChromeOptions options;
 
+   
     //Driver Id, 경로(chrome driver.exe가 있는 path)
     public static final String WEB_DRIVER_ID = "webdriver.chrome.driver";
     public static final String WEB_DRIVER_PATH = "C:\\Users\\dnwls\\OneDrive\\바탕 화면\\셀레니움\\chromedriver_win32/chromedriver.exe";
 
-	public static void main(String[] args) {
+
+	public static void main(String[] args) throws InterruptedException {
     	crawlerSet();
-    	startCrawler();
+    	login();
+    	AnnouncementsCrawler();
     }
 	
+
 	public static void crawlerSet() {
 		//WebDriver 경로 설정
         try {
-		System.setProperty(WEB_DRIVER_ID, WEB_DRIVER_PATH);
+			System.setProperty(WEB_DRIVER_ID, WEB_DRIVER_PATH);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 
+        
         //옵션 설정
         options = new ChromeOptions();
         options.addArguments("--start-maximized"); //전체화면으로 실행
@@ -44,13 +52,28 @@ public class mainCrawler {
         options.addArguments("--disable-default-apps"); //무조건 크롬으로 시작
         //
 
+        
         //WebDriver 객체 생성 = 하나의 브라우저 창
         driver = new ChromeDriver(options); //크롬 브라우저 실행
 	}
 
-    public static void startCrawler() {
+	public static void login() throws InterruptedException {		
+		String main_url = "https://mportal.ajou.ac.kr/main.do";
+		driver.get(main_url);
+		Thread.sleep(1000);
 
-		for (int i = 0, cnt=0 ; i < 100; i+=10, cnt=0) {
+		driver.findElement(By.xpath("//*[contains(concat( \" \", @class, \" \" ), concat( \" \", \"nb-h-icon-signIn\", \" \" ))]//a")).click();
+		Thread.sleep(1000);
+		//*[(@id = "P031")]//*[contains(concat( " ", @class, " " ), concat( " ", "nb-p-iconHone", " " ))]//a
+
+		driver.findElement(By.id("userId")).sendKeys("yourId");
+		driver.findElement(By.id("password")).sendKeys("yourPw");
+
+		driver.findElement(By.id("loginSubmit")).click();
+	}
+
+    public static void AnnouncementsCrawler() throws InterruptedException{
+    	for (int i = 0, cnt=0 ; i < 100; i+=10, cnt=0) {
 
 			String url_title = "https://www.ajou.ac.kr/kr/ajou/notice.do?mode=list&&articleLimit=10&article.offset="+i;
 
@@ -58,19 +81,16 @@ public class mainCrawler {
 			try {
 				// get page -> 브라우저에서 url을 주소창에 넣은 후 request 한 것과 같다
 				driver.get(url_title);
-
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
-
 			List<WebElement> Elements_num = driver.findElements(By.xpath("//*[contains(concat( \" \", @class, \" \" ), concat( \" \", \"b-num-box\", \" \" ))]"));
 			List<WebElement> Elements_title = driver.findElements(By.xpath("//*[contains(concat( \" \", @class, \" \" ), concat( \" \", \"b-title-box\", \" \" ))]//a"));
-
 			for (WebElement num : Elements_num) {
 				String article_num = num.getText().trim();
 				String title = Elements_title.get(cnt++).getText().trim();				
 				System.out.println(article_num+" "+title);
 			}
 		}
-    }
-}  
+    } 
+} 
